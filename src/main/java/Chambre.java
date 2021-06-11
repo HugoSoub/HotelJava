3308d26b3947d1fbfa1cf000b9eb0d180fba7fdc
Hugo <hugosoubey@gmail.com>
2021-06-11 17:15:06 +0200

    class chambre

---
+++ src/main/java/Chambre.java
@@ -0,0 +1,66 @@
+import javax.persistence.*;
+
+/**
+ * @author Hugo Soubeyrat
+ * @since 11/06/2021
+ */
+@Entity
+@Table(name = "Chambre")
+public class Chambre {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "type", nullable = true)
+    private int type;
+
+    @Column(name = "superficie", nullable = true)
+    private double superficie;
+
+    @Column(name = "num_chambre", length = 128, nullable = false)
+    private int num_chambre;
+
+    @Column(name = "etage", nullable = false)
+    private int etage;
+
+    public Chambre(int num_chambre, double superficie, int type, int etage) {
+        this.num_chambre = num_chambre;
+        this.superficie = superficie;
+        this.type = type;
+        this.etage = etage;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getEtage() {
+        return etage;
+    }
+
+    public void setEtage(int etage) {
+        this.etage = etage;
+    }
+
+    public int getNum_chambre() {
+        return num_chambre;
+    }
+
+    public void setNum_chambre(int num_chambre) {
+        this.num_chambre = num_chambre;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+}
